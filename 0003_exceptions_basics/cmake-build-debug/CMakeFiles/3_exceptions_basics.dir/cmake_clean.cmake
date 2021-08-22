c72530c71d86a6151545fc8cdf46890615c0de40
sla-ppy <dev@sla-ppy.com>
2021-08-22 20:33:55 +0200

    renamed file names so github sorts episodes correctly

---
+++ 0003_exceptions_basics/cmake-build-debug/CMakeFiles/3_exceptions_basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3_exceptions_basics"
+  "3_exceptions_basics.pdb"
+  "CMakeFiles/3_exceptions_basics.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_exceptions_basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
